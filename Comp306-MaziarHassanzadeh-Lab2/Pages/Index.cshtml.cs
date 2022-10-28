7e08776a9cf4ba4824b66675b35d7441fb561faf
Maziar Hassanzadeh <mhassanz@my.centennialcollege.ca>
2022-10-28 14:43:52 -0400

    Add project files.

---
+++ Comp306-MaziarHassanzadeh-Lab2/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comp306_MaziarHassanzadeh_Lab2.Pages
+{
+    public class IndexModel 
+    {
+        public string Student { get; set; }
+        public DateTime TheTime { get; set; } = DateTime.Now;
+    }
+}
